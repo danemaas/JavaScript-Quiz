a96ee54a904e9449bd0947c36168599b9648c5c7
danemaas <danemaas16@gmail.com>
2023-10-02 13:22:13 +0800

    initial commit

---
+++ LongestDynasty.js
@@ -0,0 +1,70 @@
+const dynastyReign = [
+    {"San Dynasty": "MXLI"},
+    {"Viloria Dynasty": "MCCCIIII"},
+    {"Tan Dynasty": "MCCCXCVIII"},
+    {"Bon Dynasty": "MCDXLV"},
+    {"Maiko Dynasty": "MDCLXIV"},
+    {"Paul Dynasty": "MCMXLIX"},
+    {"Andre Dynasty": "MMMXICX"},
+];
+
+const longest = longestdynasty(dynastyReign);
+console.log(longest);
+
+// longestdynasty(dynastyReign);
+
+function longestdynasty(dynastyReign) {
+    if(dynastyReign.length === 0) {
+        return "No Data";
+    } else {
+        let longest = '';
+        let maxReign = 0;
+        let currentStartYear = 1000;
+      
+        dynastyReign.forEach((entry) => {
+            const dynasty = Object.keys(entry)[0]; //store the dynasty name
+            const romanYear = entry[dynasty]; //store the roman year of the dynasty
+            const endYear = convertYear(romanYear); //convert the roman year to its numerical value
+
+            if(endYear === "Invalid") {
+                return; //if the end year of the dynasty is invalid do nothing
+            } else {
+                //compute the duration of the dynasty reign
+                const reignDuration = endYear - currentStartYear + 1;
+        
+                //if reignDuration of the current dynasty is greater than the max reign value
+                //store the reignDuration to be the maxReign value and longest will have the dysnasty name
+                if (reignDuration > maxReign) {
+                    maxReign = reignDuration;
+                    longest = dynasty;
+                }
+          
+                //update the starting year for the next dynasty
+                currentStartYear = endYear + 1;
+            }
+        });
+        
+        //return the name of the longest dynasty reign or return invalid if all data are invalid
+        return longest || "Invalid";
+    }
+}
+
+function convertYear (year) {
+    const romanFormat = /^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$/;
+    
+    if (!romanFormat.test(year)) {
+        return "Invalid"; //check for invalid characters and format
+    } else {
+        const romanNumerals = {
+            I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000
+        };
+    
+        //map the array to split its char and return the numerical values of each letter
+        const values = year.split('').map(char => romanNumerals[char]);
+    
+        return values.reduce((acc, value, index, arr) => {
+          const nextValue = arr[index + 1] || 0;
+          return value < nextValue ? acc - value : acc + value;
+        }, 0);
+    }
+}
---
+++ SalesProblem.js
@@ -0,0 +1,85 @@
+// const arrObject = [
+//     {"Product A": -75}, 
+//     {"Product B": -70}, 
+//     {"Product C": 93}, 
+//     {"Product D": 5}, 
+//     {"Product E": 88}, 
+//     {"Product F": 29}
+// ];
+
+// topProduct (arrObject);
+// bottomProduct (arrObject);
+// zeroProfitProduct (arrObject);
+
+//function uses sort and map of to iterate
+//through the array of object and generate the product with highest sales profit
+function topProduct (productProfitArray) {
+    if(productProfitArray.length === 0) {
+        return "No Data";
+    } else {
+        let copyOfArray = [...productProfitArray]; //make a copy of the array
+
+        //sort the array by their value from least to greatest then map its object keys
+        //and store it in a variable
+        const sortedProductKey = copyOfArray.sort((a, b) => 
+                Object.values(a)[0] - Object.values(b)[0]).map(obj => Object.keys(obj)[0]);
+
+        //display the last array value which has the greatest value
+        return sortedProductKey[sortedProductKey.length-1]
+    }
+}
+
+//function uses for.. of to iterate
+//through the array of object and generate the product with lowest sales profit
+function bottomProduct (productProfitArray) {
+    if(productProfitArray.length === 0) {
+        return "No Data";
+    } else {
+        //initialized first the minimum values for object key and object value
+        let minProfitKey = Object.keys(productProfitArray[0])[0];
+        let minProfitValue = Object.values(productProfitArray[0])[0];
+
+        //loop through the array of object
+        for (const obj of productProfitArray) {
+            //if value is less than the initial minimum value
+            //then store the value and key to the initial values
+            if (Object.values(obj)[0] < minProfitValue) {
+                minProfitValue = Object.values(obj)[0];
+                minProfitKey = Object.keys(obj)[0];
+            }
+        }
+
+        //display the minimum key base on its value
+        return minProfitKey;
+    }
+}
+
+//function uses conventional for loop to iterate
+//through the array of object and generate the product with sales profit closest to 0
+function zeroProfitProduct (productProfitArray) {
+    if(productProfitArray.length === 0) {
+        return "No Data";
+    } else {
+        let closestKey;
+        let closestValue = Infinity; //initialize using the largest value posible
+
+        //iterate through the array to find the key with the value closest to zero
+        for (let i = 0; i < arrObject.length; i++) {
+            const obj = arrObject[i];
+            const key = Object.keys(obj)[0];
+            const value = Object.values(obj)[0];
+            
+            //check if the absolute value of the current value is closer to zero
+            if (Math.abs(value) < Math.abs(closestValue)) {
+                closestKey = key;
+                closestValue = value;
+            } else if (Math.abs(value) === Math.abs(closestValue) && value > closestValue) {
+                //if there are two equally close values, choose the positive one
+                closestKey = key;
+                closestValue = value;
+            }
+        }
+
+        return closestKey;
+    }
+}
